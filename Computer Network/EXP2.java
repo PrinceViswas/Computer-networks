2699e5f081a6634ae3e6db6ec6639e5979bbec75
PRINCE VISWAS G <princeviswas.g.2023.cse@ritchennai.edu.in>
2025-07-30 09:15:48 +0530

    EXP2.java

--- Computer Network/EXP2.java
+++ Computer Network/EXP2.java
@@ -1,23 +1,32 @@
 import java.io.*;
 import java.net.*;
-public class SocketHTTPClient{
-    public static void main(String[] args){
-        try{
-            Socket socket = new Socket("www.martinbroadhurst.com",80);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            out.println("GET/HTTP/1.1");
-            out.println("HOST:www.martinboardhurst.com");
-            out.println("");
-            Buffered Readerin = new Buffred Reader (new InputStream Reader (socket.getInputStream()));
-            String response Line;
-            while((response Line = in.readLine())! = null){
-                System.out.println(response Line);
+
+public class SocketHTTPClient {
+    public static void main(String[] args) {
+        try {
+            // Connect to the web server on port 80
+            Socket socket = new Socket("www.martinbroadhurst.com", 80);
+
+            // Send HTTP GET request
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("GET / HTTP/1.1");
+            out.println("Host: www.martinbroadhurst.com");
+            out.println("Connection: close");
+            out.println(); // Blank line to end the request
+
+            // Read the response
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String responseLine;
+            while ((responseLine = in.readLine()) != null) {
+                System.out.println(responseLine);
             }
+
+            // Close resources
             in.close();
             out.close();
             socket.close();
-        } catch(Exception e){
-            e.printStackTrace():
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
