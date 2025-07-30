8908c30453bf8145204d6b048b093fbb1561aeac
PRINCE VISWAS G <princeviswas.g.2023.cse@ritchennai.edu.in>
2025-07-30 09:18:40 +0530

    EXP3(a).java

--- Computer Network/EXP3(a).java
+++ Computer Network/EXP3(a).java
@@ -3,58 +3,66 @@ import java.net.*;
 import java.util.*;
 
 public class ChatServer {
-    public static void main(String args[]) {
-        try {
-            ServerSocket ss = new ServerSocket(6666);
-            Socket s = ss.accept();
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            Scanner input = new Scanner(System.in);
-            String senddata = "";
-            String receivedata = "";
-            while (!receivedata.equals("stop")) {
-                receivedata = din.readUTF();
-                System.out.println("CLIENT SAYS: " + receivedata);
-                System.out.println("TO CLIENT:");
-                senddata = input.nextLine();
-                dout.writeUTF(senddata);
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(6666);
+             Socket socket = serverSocket.accept();
+             DataInputStream din = new DataInputStream(socket.getInputStream());
+             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+             Scanner input = new Scanner(System.in)) {
+
+            String sendData = "";
+            String receiveData = "";
+
+            while (!receiveData.equalsIgnoreCase("stop")) {
+                receiveData = din.readUTF();
+                System.out.println("CLIENT SAYS: " + receiveData);
+
+                if (!receiveData.equalsIgnoreCase("stop")) {
+                    System.out.print("TO CLIENT: ");
+                    sendData = input.nextLine();
+                    dout.writeUTF(sendData);
+                }
             }
-            din.close();
-            dout.close();
-            s.close();
-            ss.close();
-        } catch (Exception e) {
-            System.out.println(e);
+
+            System.out.println("Chat ended by client.");
+
+        } catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
-
 
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class ChatClient {
-    public static void main(String arg[]) {
-        try {
-            Socket s = new Socket("localhost", 6666);
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            Scanner input = new Scanner(System.in);
-            String senddata = "";
-            String receivedata = "";
-            while (!senddata.equals("stop")) {
-                System.out.println("TO SERVER:");
-                senddata = input.nextLine();
-                dout.writeUTF(senddata);
-                receivedata = din.readUTF();
-                System.out.println("SERVER SAYS: " + receivedata);
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 6666);
+             DataInputStream din = new DataInputStream(socket.getInputStream());
+             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+             Scanner input = new Scanner(System.in)) {
+
+            String sendData = "";
+            String receiveData = "";
+
+            while (!sendData.equalsIgnoreCase("stop")) {
+                System.out.print("TO SERVER: ");
+                sendData = input.nextLine();
+                dout.writeUTF(sendData);
+
+                if (!sendData.equalsIgnoreCase("stop")) {
+                    receiveData = din.readUTF();
+                    System.out.println("SERVER SAYS: " + receiveData);
+                }
             }
-            din.close();
-            dout.close();
-            s.close();
-        } catch (Exception e) {
-            System.out.println(e);
+
+            System.out.println("Chat ended by client.");
+
+        } catch (IOException e) {
+            System.err.println("Client error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
